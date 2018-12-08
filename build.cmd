dfa9de15daf2463a7456bf813d3f373d3025f024
unknown <donatopirozzi@gmail.com>
2018-12-08 11:13:19 +0100

    first draft of the ES6 module implementation (issue #8)

--- build.cmd
+++ build.cmd
@@ -1,4 +1,14 @@
 del csvjson.js
+del csvjson.mjs
 del csvjson.min.js
+
+copy src\csvjson.js csvjson.mjs
+echo & echo[ >> csvjson.mjs
+echo & echo[ >> csvjson.mjs
+echo & echo[export default csvjson; >> csvjson.mjs
+
 copy src\csvjson.js csvjson.js
 uglifyjs csvjson.js --compress -o csvjson.min.js
+
+
+ 
